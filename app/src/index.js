a4b410440843f388a82821e0875727d8cfbd7bef
Martin Bechev <martinbechev@gmail.com>
2018-10-10 23:26:22 +0300

    Print result from convertion and change fav icon

--- app/src/index.js
+++ app/src/index.js
@@ -2,25 +2,49 @@ import { listCurrencies, currenciesArray } from "./database.js";
 import { autocomplete } from "./autocomplete.js";
 import * as $ from "jquery";
 
-console.log(listCurrencies);
-
-
-// autocomplete($("#myInput"), currenciesArray);
 autocomplete(document.getElementById("from-input"), currenciesArray);
 autocomplete(document.getElementById("to-input"), currenciesArray);
 
-let from = "";
-let to = "";
 
 $("#convert-btn").click(function () {
-    from = ($("#from-input").val()); //USD
-    to = ($("#to-input").val()); //BGN
+    let from = ($("#from-input").val()); //USD
+     let to = ($("#to-input").val()); //BGN
     let url = `https://free.currencyconverterapi.com/api/v6/convert?q=${from}_${to}`;
 
-    $.get(url, function (data) {
-        console.log(data);
-    })
-})
+    let getExchangeRate = () => {
+        return new Promise((resolve) => {
+            $.get(url, (data) => {
+                let printVal = (obj) => {
+                    if (obj.val) {
+                        resolve(obj.val)
+                    } else {
+                        for (const key in obj) {
+                            printVal(obj[key]);
+                        }
+                    }
+                }
+                printVal(data);
+            });
+        })
+    }
+
+    let convertionResult = (rate) => {
+        return new Promise((resolve) => {
+            let amount = $("#amount").val();
+            let calculation = (amount * rate).toFixed(4);
+            resolve(calculation);
+        })
+    }
+
+    let aSync = async function () {
+        let exchangeRate = await getExchangeRate();
+        let result = await convertionResult(exchangeRate);
+
+        let tag = $("<p></p>").text(result).css("color", "white");
+        $("#result").append(tag);
+    }
+    aSync();
+});
 
 $("#swap-btn").click(function () {
     let from = ($("#from-input").val());
