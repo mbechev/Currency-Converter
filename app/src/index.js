afbbc7538b7e15c4aaa9f9b508a7d7c0261ba34d
Martin Bechev <martinbechev@gmail.com>
2018-10-11 11:45:51 +0300

    Add ESlint config

--- app/src/index.js
+++ app/src/index.js
@@ -1,64 +1,69 @@
-import { listCurrencies, currenciesArray } from "./database.js";
-import { autocomplete } from "./autocomplete.js";
-import * as $ from "jquery";
+import {
+  listCurrencies,
+  currenciesArray,
+} from './database.js';
+import {
+  autocomplete,
+} from './autocomplete.js';
+import * as $ from 'jquery';
 
-autocomplete(document.getElementById("from-input"), currenciesArray);
-autocomplete(document.getElementById("to-input"), currenciesArray);
+autocomplete(document.getElementById('from-input'), currenciesArray);
+autocomplete(document.getElementById('to-input'), currenciesArray);
 
 
-$("#convert-btn").click(function () {
-    let from = ($("#from-input").val()); //USD
-     let to = ($("#to-input").val()); //BGN
-    let url = `https://free.currencyconverterapi.com/api/v6/convert?q=${from}_${to}`;
+$('#convert-btn').click(function() {
+  const from = ($('#from-input').val()); // USD
+  const to = ($('#to-input').val()); // BGN
+  const url = `https://free.currencyconverterapi.com/api/v6/convert?q=${from}_${to}`;
 
-    let getExchangeRate = () => {
-        return new Promise((resolve) => {
-            $.get(url, (data) => {
-                let printVal = (obj) => {
-                    if (obj.val) {
-                        resolve(obj.val)
-                    } else {
-                        for (const key in obj) {
-                            printVal(obj[key]);
-                        }
-                    }
-                }
-                printVal(data);
-            });
-        })
-    }
+  const getExchangeRate = () => {
+    return new Promise((resolve) => {
+      $.get(url, (data) => {
+        const printVal = (obj) => {
+          if (obj.val) {
+            resolve(obj.val)
+          } else {
+            for (const key in obj) {
+              printVal(obj[key]);
+            }
+          }
+        }
+        printVal(data);
+      });
+    });
+  };
 
-    let convertionResult = (rate) => {
-        return new Promise((resolve) => {
-            let amount = $("#amount").val();
-            let calculation = (amount * rate).toFixed(4);
-            resolve(calculation);
-        })
-    }
+  const convertionResult = (rate) => {
+    return new Promise((resolve) => {
+      const amount = $('#amount').val();
+      const calculation = (amount * rate).toFixed(4);
+      resolve(calculation);
+    });
+  };
 
-    let aSync = async function () {
-        let exchangeRate = await getExchangeRate();
-        let result = await convertionResult(exchangeRate);
-        
-        //currency symbol if any
-        let currentSymbol = listCurrencies[to].currencySymbol || to; 
+  const aSync = async function() {
+    const exchangeRate = await getExchangeRate();
+    const result = await convertionResult(exchangeRate);
 
-        /* tag append is not working here. Fixed as result paragraph text change, 
-        but we should find a better solution */
+    // currency symbol if any
+    const currentSymbol = listCurrencies[to].currencySymbol || to;
 
-        /* let tag = $("<p></p>").text(`${result} ! ! ! ${currentSymbol} !`).css("color", "white");
-        $("#result").append(tag); */
+    /* tag append is not working here. Fixed as result paragraph text change, 
+    but we should find a better solution */
 
-        $("#res-par").text(`${result} ${currentSymbol}`).css("color", "green");
-    }
-    aSync();
+    /* let tag = $('<p></p>').text(`${result} ! ! ! ${currentSymbol} !`).css('color', 'white');
+    $('#result').append(tag); */
+
+    $('#res-par').text(`${result} ${currentSymbol}`).css('color', 'green');
+  }
+  aSync();
 });
 
-$("#swap-btn").click(function () {
-    let from = ($("#from-input").val());
-    let to = ($("#to-input").val()); 
-    let newFrom = to;
-    let newTo = from;
-    $("#from-input").val(newFrom);
-    $("#to-input").val(newTo);
+$('#swap-btn').click(function() {
+  const from = ($('#from-input').val());
+  const to = ($('#to-input').val());
+  const newFrom = to;
+  const newTo = from;
+  $('#from-input').val(newFrom);
+  $('#to-input').val(newTo);
 });
