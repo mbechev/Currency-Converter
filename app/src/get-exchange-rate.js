c77dc5ab7af59447475f02e775041a1ac7dbadf9
Martin Bechev <martinbechev@gmail.com>
2018-10-13 23:30:52 +0300

    Make page to be good for eyes, and add some pretty staffs

--- app/src/get-exchange-rate.js
+++ app/src/get-exchange-rate.js
@@ -7,9 +7,10 @@ const getExchangeRate = (url, from, to) => {
   return new Promise((resolve, reject) => {
     if (!currenciesArray.includes(from) || !currenciesArray.includes(to)) {
       const errorMessage = !currenciesArray.includes(from) ?
-        'Invalid currency, in "from" input filed' :
-        'Invalid currency, in "to" input field';
-      const printMEssage = $('#res-par').text(`${errorMessage}`).css('color', 'red');
+        'Invalid currency, in "from" input filed.' :
+        'Invalid currency, in "to" input field.';
+      const printMEssage = $('#res-par')
+          .text(`${errorMessage}`).css('color', 'red');
       reject(printMEssage);
     }
     $.get(url, (data) => {
