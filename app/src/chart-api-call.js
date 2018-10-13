0d0cf612544b0a6b8345a0f463e161c870ed6562
Nikolai Vassilev <nikolaivassilev@gmail.com>
2018-10-13 13:37:44 +0300

    chart modules and chart event for different ranges

--- app/src/chart-api-call.js
+++ app/src/chart-api-call.js
@@ -6,13 +6,13 @@ const getHistoricalData = (days, from, to) => {
     const toDate = today.toISOString().split('T')[0];
     today.setDate(today.getDate() - (days-1));
     const fromDate = today.toISOString().split('T')[0];
-    console.log(toDate);
-    console.log(fromDate);
+    // console.log(toDate);
+    // console.log(fromDate);
 
     const url = `http://currencies.apps.grandtrunk.net/getrange/${fromDate}/${toDate}/${from.toLowerCase()}/${to.toLowerCase()}`;
     return new Promise((resolve) => {
       $.get(url, (data) => {
-        console.log(data);
+        // console.log(data);
         resolve(data);
       });
     });
